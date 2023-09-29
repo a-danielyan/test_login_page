8618605950222948692a95884b63eb7a8479ff2f
a-danielyan <danielyan.arman.9801@gmail.com>
2023-09-29 12:17:43 +0400

    showed houses data in posts page

---
+++ app/data/beachHousesData.ts
@@ -0,0 +1,126 @@
+import house_img from '../../public/house.jpg';
+import house_img1 from '../../public/house-interior-1.jpg';
+import house_img2 from '../../public/house-interior-2.jpg';
+
+export const beachHouses = [
+    {
+        name: "Beach House in Collingwood 1",
+        type: "Entire house 1",
+        rating: 4.89,
+        location: "Collingwood, Ontario 1",
+        description: "This sunny and spacious room is for those traveling light and looking for a comfy and cosy place to lay their head for a night or two. This beach house sits in a vibrant neighborhood littered with cafes, pubs, restaurants and supermarkets and is close to all the major attractions such as Edinburgh Castle and Arthur's Seat.",
+        images: [
+            { src: house_img },
+            { src: house_img1 },
+            { src: house_img2 },
+        ],
+    },
+    {
+        name: "Beach House in Collingwood 2",
+        type: "Entire house 2",
+        rating: 4.89,
+        location: "Collingwood, Ontario 2",
+        description: "This sunny and spacious room is for those traveling light and looking for a comfy and cosy place to lay their head for a night or two. This beach house sits in a vibrant neighborhood littered with cafes, pubs, restaurants and supermarkets and is close to all the major attractions such as Edinburgh Castle and Arthur's Seat.",
+        images: [
+            { src: house_img },
+            { src: house_img1 },
+            { src: house_img2 },
+        ],
+    },
+    {
+        name: "Beach House in Collingwood 3",
+        type: "Entire house 3",
+        rating: 4.89,
+        location: "Collingwood, Ontario 3",
+        description: "This sunny and spacious room is for those traveling light and looking for a comfy and cosy place to lay their head for a night or two. This beach house sits in a vibrant neighborhood littered with cafes, pubs, restaurants and supermarkets and is close to all the major attractions such as Edinburgh Castle and Arthur's Seat.",
+        images: [
+            { src: house_img },
+            { src: house_img1 },
+            { src: house_img2 },
+        ],
+    },
+    {
+        name: "Beach House in Collingwood 3",
+        type: "Entire house 3",
+        rating: 4.89,
+        location: "Collingwood, Ontario 3",
+        description: "This sunny and spacious room is for those traveling light and looking for a comfy and cosy place to lay their head for a night or two. This beach house sits in a vibrant neighborhood littered with cafes, pubs, restaurants and supermarkets and is close to all the major attractions such as Edinburgh Castle and Arthur's Seat.",
+        images: [
+            { src: house_img },
+            { src: house_img1 },
+            { src: house_img2 },
+        ],
+    },
+    {
+        name: "Beach House in Collingwood 4",
+        type: "Entire house 4",
+        rating: 4.89,
+        location: "Collingwood, Ontario 4",
+        description: "This sunny and spacious room is for those traveling light and looking for a comfy and cosy place to lay their head for a night or two. This beach house sits in a vibrant neighborhood littered with cafes, pubs, restaurants and supermarkets and is close to all the major attractions such as Edinburgh Castle and Arthur's Seat.",
+        images: [
+            { src: house_img },
+            { src: house_img1 },
+            { src: house_img2 },
+        ],
+    },
+    {
+        name: "Beach House in Collingwood 5",
+        type: "Entire house 5",
+        rating: 4.89,
+        location: "Collingwood, Ontario 5",
+        description: "This sunny and spacious room is for those traveling light and looking for a comfy and cosy place to lay their head for a night or two. This beach house sits in a vibrant neighborhood littered with cafes, pubs, restaurants and supermarkets and is close to all the major attractions such as Edinburgh Castle and Arthur's Seat.",
+        images: [
+            { src: house_img },
+            { src: house_img1 },
+            { src: house_img2 },
+        ],
+    },
+    {
+        name: "Beach House in Collingwood 6",
+        type: "Entire house 6",
+        rating: 4.89,
+        location: "Collingwood, Ontario 6",
+        description: "This sunny and spacious room is for those traveling light and looking for a comfy and cosy place to lay their head for a night or two. This beach house sits in a vibrant neighborhood littered with cafes, pubs, restaurants and supermarkets and is close to all the major attractions such as Edinburgh Castle and Arthur's Seat.",
+        images: [
+            { src: house_img },
+            { src: house_img1 },
+            { src: house_img2 },
+        ],
+    },
+    {
+        name: "Beach House in Collingwood 7",
+        type: "Entire house 7",
+        rating: 4.89,
+        location: "Collingwood, Ontario 7",
+        description: "This sunny and spacious room is for those traveling light and looking for a comfy and cosy place to lay their head for a night or two. This beach house sits in a vibrant neighborhood littered with cafes, pubs, restaurants and supermarkets and is close to all the major attractions such as Edinburgh Castle and Arthur's Seat.",
+        images: [
+            { src: house_img },
+            { src: house_img1 },
+            { src: house_img2 },
+        ],
+    },
+    {
+        name: "Beach House in Collingwood 8",
+        type: "Entire house 8",
+        rating: 4.89,
+        location: "Collingwood, Ontario 8",
+        description: "This sunny and spacious room is for those traveling light and looking for a comfy and cosy place to lay their head for a night or two. This beach house sits in a vibrant neighborhood littered with cafes, pubs, restaurants and supermarkets and is close to all the major attractions such as Edinburgh Castle and Arthur's Seat.",
+        images: [
+            { src: house_img },
+            { src: house_img1 },
+            { src: house_img2 },
+        ],
+    },
+    {
+        name: "Beach House in Collingwood 9",
+        type: "Entire house 9",
+        rating: 4.89,
+        location: "Collingwood, Ontario 9",
+        description: "This sunny and spacious room is for those traveling light and looking for a comfy and cosy place to lay their head for a night or two. This beach house sits in a vibrant neighborhood littered with cafes, pubs, restaurants and supermarkets and is close to all the major attractions such as Edinburgh Castle and Arthur's Seat.",
+        images: [
+            { src: house_img },
+            { src: house_img1 },
+            { src: house_img2 },
+        ],
+    },
+];
